fd96fc9f6f5e7b55eb6ab3f51dcf53245668b4f3
Dmitriy <datfeelman@gmail.com>
2021-06-25 14:10:49 +0300

    Переписано условие обработки чисел, числа должны быть от 1 до 10. В ином случае выбросить исключении во избежание деления на ноль, использования нуля и чисел выше 10

--- src/Calculator/Calc.java
+++ src/Calculator/Calc.java
@@ -6,24 +6,13 @@ public class Calc {
 
     // выполнение выражения
     private int calcExp(int n1, String op, int n2){
-        int result;
-        switch(op){
-            case "+":
-                result = n1 + n2;
-                break;
-            case "-":
-                result = n1 - n2;
-                break;
-            case "/":
-                result = n1 / n2;
-                break;
-            case "*":
-                result = n1 * n2;
-                break;
-            default:
-                throw new AssertionError();
-        }
-        return result;
+        return switch (op) {
+            case "+" -> n1 + n2;
+            case "-" -> n1 - n2;
+            case "/" -> n1 / n2;
+            case "*" -> n1 * n2;
+            default -> throw new AssertionError();
+        };
     }
 
     // метод с проверками и выводом
@@ -33,8 +22,10 @@ public class Calc {
         String[] expItems = exp.split(" ");
         //Проверка что создался массив из трех значений. Удачная проверка указывает на то что пользователь ввел числа и оператор разделив их пробелами. Иначе исключение.
         if(expItems.length != 3) {
-            throw new CalcException("Выражение должно быть формата: \"Число1 - оператор - Число2\".\nЧисла и оператор должны быть разделенны пробелом\n" +
-                    "Римские числа должны быть в верхнем регистре");
+            throw new CalcException("""
+                    Выражение должно быть формата: "Число1 - оператор - Число2".
+                    Числа и оператор должны быть разделенны пробелом
+                    Римские числа должны быть в верхнем регистре""");
         }
         // Проверка оператора, должен быть + - / *
         if (validation.correctOperator(expItems[1])){
@@ -46,19 +37,26 @@ public class Calc {
         if (validation.isArabic(expItems[0]) && validation.isArabic(expItems[2])) {
             num1 = Integer.parseInt(expItems[0]);
             num2 = Integer.parseInt(expItems[2]);
+          //  if (num1 == 0 || num2 == 0) {
+             //   throw new CalcException("Введите число от 1 до 10");
+          //  }
         } else if (validation.isRoman(expItems[0]) && validation.isRoman(expItems[2])) {
             num1 = validation.romanToArabic(expItems[0]);
             num2 = validation.romanToArabic(expItems[2]);
+           // if (num1 == 0 || num2 == 0) {
+              //  throw new CalcException("Введите число от 1 до 10");
+           // }
             isRomanExp = true;
         } else {
             throw new CalcException("Числа должны быть целыми.\nЧисла должны быть оба римские или оба арабские.\nРимские числа должны быть в верхнем регистре");
         }
-        if (!(num1 >= 1 && num1 <= 10) && !(num2 >= 1 && num2 <= 10)) {
+        // Обработка нулей и чисел больше 10
+        if (!(num1 > 0 && num1 <= 10) || !(num2 > 0 && num2 <= 10)) {
             throw new CalcException("Числа должны быть от 1 до 10 включительно");
         }
         // Получаем результат
         int result = calcExp(num1, operator, num2);
-
+        // Если числа на входе были римские, вернуть результат в римском представлении.
         if (isRomanExp) {
             String sign = result < 0 ? "-" : "";
             return sign + validation.arabicToRoman(Math.abs(result));
